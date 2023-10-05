e8f71344639711855af6a20c8ddd80151d00a748
NewellSkk <newellsikuku@gmail.com>
2023-10-05 10:26:22 +0000

    mcv changes

--- controllers/products.js
+++ controllers/products.js
@@ -10,17 +10,16 @@ exports.getAddProduct=(req,res,next)=>{
 exports.postAddProducts=(req,res,next)=>{
     const product = new Product(req.body.title);
     product.save();
-    res.redirect("/");
+    res.redirect('/');
 };
 
 exports.getProducts=(req,res,next)=>{
+    
     Product.fetchAll(products=>{
         res.render('shop',
         {
             prods:products,
             pageTitle:"Shop",
-        }
-    )
-    })
-    
+        });
+    });
 }
