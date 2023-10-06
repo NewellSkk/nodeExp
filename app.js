329a90ded6cd942de570fab14a738170b45079fa
NewellSkk <newellsikuku@gmail.com>
2023-10-06 14:30:04 +0000

    mod

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
-const path = require('path')
-const express = require('express')
-const bodyParser = require('body-parser')
+const path = require('path');
 
-const errorController =  require('./controllers/error')
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const adminRoutes = require('./routes/admin')
-const shopRoutes = require('./routes/shop')
+const errorController = require('./controllers/error');
 
 const port = 8000
-const app = express()
+const app = express();
 
-//CONFIGURE TEMPLATING ENGINE
-app.set('view engine','ejs')
-//CONFIGURE VIEWS FOLDER PATH
-app.set('views','views')
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-app.use(bodyParser.urlencoded({extended:false}))
-app.use(express.static(path.join(__dirname,"public")))//serving statically
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use('/admin',adminRoutes)
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/',shopRoutes)
- 
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
 app.use(errorController.get404);
 
-app.listen(8000,()=>{console.log(`server1 listening on port ${port}`)})
+app.listen(port,()=>{
+    console.log(`Listening on port ${port}`)
+});
--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,19 @@
-const express = require('express')
+const path = require('path');
 
-const productsController = require('../controllers/products')
+const express = require('express');
 
-const Router = express.Router();
+const shopController = require('../controllers/shop');
 
-Router.get("/",productsController.getProducts);
+const router = express.Router();
 
-module.exports=Router
+router.get('/', shopController.getIndex);
+
+router.get('/products', shopController.getProducts);
+
+router.get('/cart', shopController.getCart);
+
+router.get('/orders', shopController.getOrders);
+
+router.get('/checkout', shopController.getCheckout);
+
+module.exports = router;
