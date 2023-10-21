2c05a2eea6d2a256c298433d7de2bf548524f3e7
NewellSkk <newellsikuku@gmail.com>
2023-10-21 22:07:39 +0000

    ...

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
@@ -9,6 +10,19 @@ exports.getProducts = (req, res, next) => {
     });
   });
 };
+
+exports.getProduct = (req, res, next) =>{
+  const prodID= req.params.productId;
+  Product.findById(prodID,product=>{
+    res.render('shop/product-detail',
+      {
+        product: product,
+        pageTitle: product.title,
+        path:'/products'
+      }
+    );
+  });
+}
 
 exports.getIndex = (req, res, next) => {
   Product.fetchAll(products => {
@@ -21,11 +35,34 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  res.render('shop/cart', {
-    path: '/cart',
-    pageTitle: 'Your Cart'
-  });
+  Cart.getCart(cart=>{
+    Product.fetchAll(products=>{
+      const cartProducts=[];
+      for(product of products){
+        const cartProductData=cart.products.find(prod=>prod.id === product.id)
+        if(cartProductData){
+          cartProducts.push({productData:product,qty:cartProductData.qty})
+        }
+      }
+      res.render('shop/cart', {
+        path: '/cart',
+        pageTitle: 'Your Cart',
+        products:cartProducts
+      });
+    })
+   
+  })
+ 
 };
+
+exports.postCart = (req,res,next) => {
+  const prodID = req.body.productId;
+  Product.findById(prodID,product=>{
+    Cart.addProduct(prodID,product.price);
+  })
+  res.redirect('/cart');
+
+}
 
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
---
+++ models/cart.js
@@ -0,0 +1,68 @@
+const { Console } = require('console');
+const { json } = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(
+    path.dirname(process.mainModule.filename),
+    'data',
+    'cart.json'
+  );
+
+module.exports = class Cart{
+    static addProduct(id, productPrice){
+        fs.readFile(p,(err,fileContent)=>{
+            let cart = {products: [],totalPrice: 0}
+            if(!err){
+                cart = JSON.parse(fileContent);
+            }
+
+            const existingProductIndex = cart.products.findIndex(prod=> prod.id === id);
+            const existingProduct = cart.products[existingProductIndex];
+
+            let updatedProduct;
+            if(existingProduct){
+                updatedProduct = {...existingProduct};
+                updatedProduct.qty += 1;
+                cart.products = [...cart.products];
+                cart.products[existingProductIndex]= updatedProduct;
+            }else{
+                updatedProduct = {id: id,qty: 1};
+                cart.products = [...cart.products, updatedProduct]
+            }
+            cart.totalPrice+= +productPrice;
+            fs.writeFile(p,JSON.stringify(cart),err=>{
+                console.log(err);
+            })
+           
+        });
+    }
+    static deleteProduct(id,price){
+        console.log("deleteProduct fn")
+        fs.readFile(p,(err,fileContent)=>{
+            if(err){
+                console.log("Read file error")
+                return;
+            }
+            console.log("This works");
+            const updatedCart = {...JSON.parse(fileContent)};
+            const product = updatedCart.products.find(prod=> prod.id === id);
+            updatedCart.products = updatedCart.products.filter(prod=> prod.id!==id);
+            updatedCart.totalPrice -= product.qty*price; 
+            fs.writeFile(p,JSON.stringify(updatedCart),err=>{
+                console.log(err);
+            })
+
+        })
+    }
+    static getCart(cb){
+        fs.readFile(p,(err,fileContent)=>{
+            if(err){
+                cb(null);
+            }else{
+            const cart = JSON.parse(fileContent);
+            cb(cart);
+            } 
+        })
+    }
+}
